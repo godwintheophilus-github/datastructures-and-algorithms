dd5bcb4e945d5215825e137c58f36c603fbf5517
gdwntheophilus <85812329+gdwntheophilus@users.noreply.github.com>
2024-07-15 21:50:42 +0530

    tupples

--- datastructures/tuples/tuples.py
+++ datastructures/tuples/tuples.py
@@ -1,4 +1,13 @@
 # create a tuple in python
 
-test_tupple = (1,2,3,4,5,6,11,11,11,11)
-print(test_tupple)
+test_tupple = (1,2,3,4,5,6,11,11,11,'11')
+print(test_tupple) # output (1, 2, 3, 4, 5, 6, 11, 11, 11, 11)
+
+# identify the index of the tuple
+print(test_tupple.index(11)) # output 6
+
+# identify the count of element in the tuple
+print(test_tupple.count(11)) # output 4
+
+# retrive data based on index
+print(test_tupple[4]) # output 5
